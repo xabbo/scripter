53e2b640f5d2e2373e3d6ec70199e19bf49aace3
b7c <58299468+b7c@users.noreply.github.com>
2021-07-20 00:41:49 +1200

    add marketplace methods

--- b7.Scripter.Common/Scripting/G.cs
+++ b7.Scripter.Common/Scripting/G.cs
@@ -1834,6 +1834,48 @@ namespace b7.Scripter.Scripting
             => new GetCatalogPageTask(Interceptor, pageId, mode).Execute(timeout, Ct);
         #endregion
 
+        #region - Marketplace -
+        /// <summary>
+        /// Gets the user's own marketplace offers.
+        /// </summary>
+        /// <param name="timeout">The time in milliseconds to wait for a response from the server.</param>
+        public IUserMarketplaceOffers GetUserMarketplaceOffers(int timeout = DEFAULT_TIMEOUT)
+            => new GetUserMarketplaceOffersTask(Interceptor).Execute(timeout, Ct);
+
+        /// <summary>
+        /// Searches for open offers in the marketplace.
+        /// </summary>
+        /// <param name="searchText">The name of the item to search for.</param>
+        /// <param name="from">The minimum offer price in credits.</param>
+        /// <param name="to">The maximum offer price in credits.</param>
+        /// <param name="sort">The order in which to sort the results.</param>
+        /// <param name="timeout">The time in milliseconds to wait for a response from the server.</param>
+        /// <returns>The list of matching marketplace offers.</returns>
+        public IEnumerable<IMarketplaceOffer> SearchMarketplace(string? searchText = null,
+            int? from = null, int? to = null,
+            MarketplaceSortOrder sort = MarketplaceSortOrder.HighestPrice,
+            int timeout = DEFAULT_TIMEOUT)
+            => new SearchMarketplaceTask(Interceptor, searchText, from, to, sort).Execute(timeout, Ct);
+
+        /// <summary>
+        /// Gets the marketplace information for the specified item type and kind.
+        /// </summary>
+        public IMarketplaceItemInfo GetMarketplaceInfo(ItemType type, int kind, int timeout = DEFAULT_TIMEOUT)
+            => new GetMarketplaceInfoTask(Interceptor, type, kind).Execute(timeout, Ct);
+
+        /// <summary>
+        /// Gets the marketplace information for the specified item.
+        /// </summary>
+        public IMarketplaceItemInfo GetMarketplaceInfo(IItem item, int timeout = DEFAULT_TIMEOUT)
+            => new GetMarketplaceInfoTask(Interceptor, item.Type, item.Kind).Execute(timeout, Ct);
+
+        /// <summary>
+        /// Gets the marketplace information for the specified furni.
+        /// </summary>
+        public IMarketplaceItemInfo GetMarketplaceInfo(FurniInfo furniInfo, int timeout = DEFAULT_TIMEOUT)
+            => new GetMarketplaceInfoTask(Interceptor, furniInfo.Type, furniInfo.Kind).Execute(timeout, Ct);
+        #endregion
+
         #region - Events -
         private void Register<TEventSource, TEventArgs>(TEventSource source, string eventName, Func<TEventArgs, Task> callback)
             where TEventArgs : EventArgs
