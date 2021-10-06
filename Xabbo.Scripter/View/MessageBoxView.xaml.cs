282e78644582547edf3ac6cf4c4fa4abeaf36f6b
b7c <58299468+b7c@users.noreply.github.com>
2021-10-06 16:39:13 +1300

    move message box view to its own component

---
+++ Xabbo.Scripter/View/MessageBoxView.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace Xabbo.Scripter.View
+{
+    public partial class MessageBoxView : UserControl
+    {
+        public MessageBoxView()
+        {
+            InitializeComponent();
+        }
+    }
+}
