8d11aa1f09ac3685b631d5a2da199355003491be
b7c <58299468+b7c@users.noreply.github.com>
2022-07-08 09:25:57 +1200

    Add PageService

---
+++ Xabbo.Scripter/Services/PageService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using Wpf.Ui.Mvvm.Contracts;
+
+namespace Xabbo.Scripter.Services;
+
+internal class PageService : IPageService
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public PageService(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    public T? GetPage<T>() where T : class => _serviceProvider.GetRequiredService<T>();
+
+    public FrameworkElement? GetPage(Type pageType) => (FrameworkElement)_serviceProvider.GetRequiredService(pageType);
+}
