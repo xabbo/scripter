795f6e07d3a9af8c752835d8aab3652c8061bf95
b7c <58299468+b7c@users.noreply.github.com>
2022-07-09 22:29:49 +1200

    UI adjustments

--- Xabbo.Scripter/View/Pages/ToolsPage.xaml.cs
+++ Xabbo.Scripter/View/Pages/ToolsPage.xaml.cs
@@ -1,27 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
+using Xabbo.Scripter.ViewModel;
 
 namespace Xabbo.Scripter.View.Pages
 {
-    /// <summary>
-    /// Interaction logic for ToolsPage.xaml
-    /// </summary>
     public partial class ToolsPage : Page
     {
-        public ToolsPage()
+        public ToolsPage(ToolsViewManager manager)
         {
+            DataContext = manager;
+
             InitializeComponent();
         }
     }
