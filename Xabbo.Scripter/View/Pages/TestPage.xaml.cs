712309fe02208644142867b2d4ec2dfb805e56fd
b7c <58299468+b7c@users.noreply.github.com>
2022-07-09 22:29:24 +1200

    Add test page

---
+++ Xabbo.Scripter/View/Pages/TestPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Xabbo.Scripter.View.Pages
+{
+    /// <summary>
+    /// Interaction logic for TestPage.xaml
+    /// </summary>
+    public partial class TestPage : Page
+    {
+        private readonly Random _rng = new();
+
+        public TestPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Resources["ApplicationBackgroundColor"] = Color.FromRgb(
+                (byte)_rng.Next(0, 256),
+                (byte)_rng.Next(0, 256),
+                (byte)_rng.Next(0, 256)
+            );
+        }
+    }
+}
