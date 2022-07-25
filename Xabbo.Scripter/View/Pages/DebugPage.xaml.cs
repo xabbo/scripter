f4d1e40ae19d3593ffdaa0b249e32dd1b4e88e1b
b7c <58299468+b7c@users.noreply.github.com>
2022-07-25 15:57:33 +1200

    Remove test button

--- Xabbo.Scripter/View/Pages/DebugPage.xaml.cs
+++ Xabbo.Scripter/View/Pages/DebugPage.xaml.cs
@@ -1,48 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace Xabbo.Scripter.View.Pages
+namespace Xabbo.Scripter.View.Pages;
+
+public partial class DebugPage : Page
 {
-    /// <summary>
-    /// Interaction logic for TestPage.xaml
-    /// </summary>
-    public partial class DebugPage : Page
+    public DebugPage()
     {
-        private readonly Random _rng = new();
+        InitializeComponent();
+    }
 
-        public DebugPage()
+    private void ButtonAttachDebugger_Click(object sender, RoutedEventArgs e)
+    {
+        if (!Debugger.IsAttached)
         {
-            InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Application.Current.Resources["ApplicationBackgroundColor"] = Color.FromRgb(
-                (byte)_rng.Next(0, 256),
-                (byte)_rng.Next(0, 256),
-                (byte)_rng.Next(0, 256)
-            );
-        }
-
-        private void ButtonAttachDebugger_Click(object sender, RoutedEventArgs e)
-        {
-            if (!Debugger.IsAttached)
-            {
-                Debugger.Launch();
-            }
+            Debugger.Launch();
         }
     }
 }
