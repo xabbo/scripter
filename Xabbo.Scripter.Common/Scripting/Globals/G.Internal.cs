76f5ee0456704295e965ec0d22567a227cfb4677
b7c <58299468+b7c@users.noreply.github.com>
2022-07-27 03:31:25 +1200

    Better exception message for null room reference when required by a global method

---
+++ Xabbo.Scripter.Common/Scripting/Globals/G.Internal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics;
+
+using Xabbo.Core.Game;
+
+namespace Xabbo.Scripter.Scripting;
+
+/*
+ * Internal helper methods used within the globals.
+ */ 
+public partial class G
+{
+    private IRoom GetRoomOrThrow() => Room ?? throw new InvalidOperationException("The user is not in a room.");
+}
