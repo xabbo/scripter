6d04d0ece4398947c92ff467e11cfe60c7a452ab
b7c <58299468+b7c@users.noreply.github.com>
2022-10-12 01:43:43 +1300

    Add GitVersionUtil

---
+++ Xabbo.Scripter/Util/GitVersionUtil.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+
+namespace Xabbo.Scripter.Util;
+
+internal static class GitVersionUtil
+{
+    public static string GetSemanticVersion()
+    {
+        return Assembly.GetExecutingAssembly()
+            .GetType("GitVersionInformation")
+            ?.GetField("SemVer")
+            ?.GetValue(null) as string
+            ?? throw new InvalidOperationException($"Failed to get SemVer from GitVersionInformation.");
+    }
+}
